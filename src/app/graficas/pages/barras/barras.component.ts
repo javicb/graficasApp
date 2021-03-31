bccd52a2c8b6fb9fe9dea039e3b402dd7ad93675
Javi <javi.cb@gmail.com>
2021-03-31 13:33:33 +0200

    peronalizando gráfica

--- src/app/graficas/pages/barras/barras.component.ts
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -26,8 +26,9 @@ export class BarrasComponent implements OnInit {
   public barChartLegend = true;
 
   public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#A43AFC', hoverBackgroundColor: 'red' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#53FDBC', hoverBackgroundColor: 'red' },
+    { data: [8, 38, 70, 59, 66, 80, 100], label: 'Series C', backgroundColor: '#FDA83D', hoverBackgroundColor: 'red' }
   ];
 
   constructor() { }
@@ -36,14 +37,31 @@ export class BarrasComponent implements OnInit {
   }
 
   public randomize(): void {
-    // Only Change 3 values
     this.barChartData[0].data = [
       Math.round(Math.random() * 100),
-      59,
-      80,
       (Math.random() * 100),
-      56,
       (Math.random() * 100),
-      40];
-  }
+      (Math.random() * 100),
+      (Math.random() * 100),
+      (Math.random() * 100),
+      (Math.random() * 100)];
+
+    this.barChartData[1].data = [
+      Math.round(Math.random() * 100),
+      (Math.random() * 100),
+      (Math.random() * 100),
+      (Math.random() * 100),
+      (Math.random() * 100),
+      (Math.random() * 100),
+      (Math.random() * 100)];
+
+    this.barChartData[2].data = [
+      Math.round(Math.random() * 100),
+      (Math.random() * 100),
+      (Math.random() * 100),
+      (Math.random() * 100),
+      (Math.random() * 100),
+      (Math.random() * 100),
+      (Math.random() * 100)];
+  };
 }
