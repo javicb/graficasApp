f877d88dfdb60cd306ff0b5505c3995ec848f9db
Javi <javi.cb@gmail.com>
2021-03-31 19:56:13 +0200

    grafica donut http

--- src/app/graficas/pages/donut-http/donut-http.component.ts
+++ src/app/graficas/pages/donut-http/donut-http.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
+import { GraficasService } from '../../serivces/graficas.service';
 
 @Component({
   selector: 'app-donut-http',
@@ -8,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonutHttpComponent implements OnInit {
 
-  constructor() { }
+  public doughnutChartLabels: Label[] = [];
+  public doughnutChartData: MultiDataSet = [];
+  public doughnutChartType: ChartType = 'doughnut';
+  public colors: Color[] = [
+    {
+      backgroundColor: [
+        '#0075ED',
+        '#00BAF7',
+        '#00E0DB',
+        '#00F7AD',
+        '#00ED63',
+      ]
+    }
+  ];
+
+  constructor(private graficasServie: GraficasService) { }
 
   ngOnInit(): void {
+    this.getData();
+  }
+
+  getData(): void {
+    this.graficasServie.getData()
+      .subscribe(
+        x => { this.doughnutChartLabels = x.labels; this.doughnutChartData.push(x.values); },
+        err => console.error('Observer got an error: ' + err),
+        () => console.log('Observer got a complete notification')
+      );
   }
 
 }
--- src/app/graficas/serivces/graficas.service.ts
+++ src/app/graficas/serivces/graficas.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GraficasService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<any> {
+    return this.http.get('http://localhost:3000/grafica')
+      .pipe(
+        delay(1500),
+        map(data => {
+          const labels = Object.keys(data);
+          const values = Object.values(data);
+          return { labels, values };
+        })
+      );
+  }
 }
