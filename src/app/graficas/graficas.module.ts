c7c55294cd28be6e3f90d6f3befece79dfdd13f6
Javi <javi.cb@gmail.com>
2021-03-31 12:52:46 +0200

    estructura del proyecto

---
+++ src/app/graficas/graficas.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GraficasRoutingModule } from './graficas-routing.module';
+import { BarrasDobleComponent } from './pages/barras-doble/barras-doble.component';
+import { BarrasComponent } from './pages/barras/barras.component';
+import { DonutComponent } from './pages/donut/donut.component';
+import { DonutHttpComponent } from './pages/donut-http/donut-http.component';
+import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.component';
+
+
+@NgModule({
+  declarations: [BarrasDobleComponent, BarrasComponent, DonutComponent, DonutHttpComponent, GraficaBarraComponent],
+  imports: [
+    CommonModule,
+    GraficasRoutingModule
+  ]
+})
+export class GraficasModule { }
